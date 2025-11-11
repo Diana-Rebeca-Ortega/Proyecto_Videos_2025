c7528cf9acc3b80028b9c754d111ef738323e414
Diana Rebeca Ortega <diana.os@tecjerez>
2025-11-11 01:34:38 -0600

    Primer commit: Se subio la base de datos de mysql worchbench y se inicio el proyecto de neatBeans

---
+++ PF_Videos_2025/src/main/java/com/mycompany/pf_videos_2025/PF_Videos_2025.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pf_videos_2025;
+
+/**
+ *
+ * @author Diana
+ */
+public class PF_Videos_2025 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
